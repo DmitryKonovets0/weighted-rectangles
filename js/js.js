4809a582c1c3bcb0ee6f55ead23b25a1501ae896
Dmitry Konovets <114726643+DmitryKonovec0@users.noreply.github.com>
2023-02-09 00:24:17 +0200

    add repeat(apply+.map)

--- js/js.js
+++ js/js.js
@@ -1,2 +1,10 @@
 let colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-colors.repeat(2) 
+
+
+// colors[3] * 10;
+// console.log(colors);
+
+
+var repeats = 1000;
+var arrayB = Array.apply(null, {length: repeats * colors.length})
+        .map(function(e,i){return colors[i % colors.length]});
--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 let canvas = document.getElementById('canvas'), // поиск документа в html
     ctx = canvas.getContext('2d'),  // стиль рисовки
     num = [10,10,10,10,10,10,10,10,10,10], // Проценты котораый занимает один блок цвета
-    colors = ['red', 'orange', 'yellow', 'green', 'blue']; //цвета которые в дальнейшем будут идти безперерывно подряд
+    color = ['red', 'orange', 'yellow', 'green', 'blue']; //цвета которые в дальнейшем будут идти безперерывно подряд
     offset = 0; // пустая переменная которая возьмет в себя num
+    repeats = 1000;
+    colors = Array.apply(null, {length: repeats * color.length})
+    .map(function(e,i){return color[i % color.length]});
 function draw() {
     for (let i = 0; i < num.length; i++){
+
+
+
         ctx.fillStyle = colors[i];
         ctx.fillRect(0,offset,200,num[i]); // можно прописать один раз и код будет производить столько раз сколько понадобиться
         offset += num[i];       // код принимает значения num и отсуп идет с 0
