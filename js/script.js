3175f7f9e43a4cd2526317bed5b22f02f9eb0933
Dmitry Konovets <114726643+DmitryKonovec0@users.noreply.github.com>
2023-02-09 00:31:47 +0200

    changed repeat(optimized)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 let canvas = document.getElementById('canvas'), // поиск документа в html
     ctx = canvas.getContext('2d'),  // стиль рисовки
-    num = [10,10,10,10,10,10,10,10,10,10], // Проценты котораый занимает один блок цвета
+    num = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], // Проценты котораый занимает один блок цвета
     color = ['red', 'orange', 'yellow', 'green', 'blue']; //цвета которые в дальнейшем будут идти безперерывно подряд
     offset = 0; // пустая переменная которая возьмет в себя num
-    repeats = 1000;
-    colors = Array.apply(null, {length: repeats * color.length})
-    .map(function(e,i){return color[i % color.length]});
 function draw() {
     for (let i = 0; i < num.length; i++){
+        let colors = color[i % color.length];
 
-
-
-        ctx.fillStyle = colors[i];
+        ctx.fillStyle = colors;
         ctx.fillRect(0,offset,200,num[i]); // можно прописать один раз и код будет производить столько раз сколько понадобиться
         offset += num[i];       // код принимает значения num и отсуп идет с 0
     }
