fe0ccd4bf788dd936afb690f7323684fafe0b622
Dmitry Konovets <114726643+DmitryKonovec0@users.noreply.github.com>
2023-02-09 02:31:06 +0200

    added all colors rainbow + moved variables inside the function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-let canvas = document.getElementById('canvas'), // поиск документа в html
-    ctx = canvas.getContext('2d'),  // стиль рисовки
-    num = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], // Проценты котораый занимает один блок цвета
-    color = ['red', 'orange', 'yellow', 'green', 'blue']; //цвета которые в дальнейшем будут идти безперерывно подряд
-    offset = 0; // пустая переменная которая возьмет в себя num
 function draw() {
+
+    let canvas = document.getElementById('canvas'), // поиск документа в html
+        ctx = canvas.getContext('2d'),  // стиль рисовки
+        num = [10,10,10,10,10,10,10,10,10,10], // Проценты котораый занимает один блок цвета
+        color = ['red', 'orange', 'yellow', 'green', 'blue','darkblue','purple'], //цвета которые в дальнейшем будут идти безперерывно подряд
+        offset = 0;
     for (let i = 0; i < num.length; i++){
-        let colors = color[i % color.length];
+       let colors = color[i % color.length]; // переменная colors забирает остаток от деления color
 
         ctx.fillStyle = colors;
         ctx.fillRect(0,offset,200,num[i]); // можно прописать один раз и код будет производить столько раз сколько понадобиться
