337fe84832842c2f94bee81d1c706647a79e033d
Dmitry Konovets <114726643+DmitryKonovec0@users.noreply.github.com>
2023-02-09 22:40:34 +0200

    added function parameters

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-function draw() {
+function draw(canvas, ctx, num, color, offset, colors ) {
 
-    let canvas = document.getElementById('canvas'), // поиск документа в html
-        ctx = canvas.getContext('2d'),  // стиль рисовки
-        num = [10,10,10,10,10,10,10,10,10,10], // Проценты котораый занимает один блок цвета
-        color = ['red', 'orange', 'yellow', 'green', 'blue','darkblue','purple'], //цвета которые в дальнейшем будут идти безперерывно подряд
+        canvas = document.getElementById('canvas'); // поиск документа в html
+        ctx = canvas.getContext('2d');  // стиль рисовки
+
+        num = [10,10,10,10,10,10,10,10,10,10]; // Проценты котораый занимает один блок цвета
+        color = ['red', 'orange', 'yellow', 'green', 'blue','darkblue','purple']; //цвета которые в дальнейшем будут идти безперерывно подряд
         offset = 0;
+
     for (let i = 0; i < num.length; i++){
-       let colors = color[i % color.length]; // переменная colors забирает остаток от деления color
+    colors = color[i % color.length]; // переменная colors забирает остаток от деления color
 
         ctx.fillStyle = colors;
         ctx.fillRect(0,offset,200,num[i]); // можно прописать один раз и код будет производить столько раз сколько понадобиться
